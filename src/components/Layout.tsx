e60d333c142955d22d0361dcc2cecdfbc47fde6a
Pawan Kumar <pawan4super30@gmail.com>
2023-11-27 16:56:48 +0530

    Created redux store....created reducers.....Configured Store to manage application.........

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,11 +1,15 @@
 "use client";
 
 import { SessionProvider } from 'next-auth/react';
+import { Provider } from 'react-redux';
+import { store } from "@/redux/store";
 
 const Layout = ({children}: {children: React.ReactNode}) => {
   return (
-    <SessionProvider>{children}</SessionProvider>
-  )
+      <Provider store={store}>
+        <SessionProvider>{children}</SessionProvider>
+      </Provider>
+    )
 }
 
 export default Layout
