cdd28cc673b65a343c4f187b2b8793ab9ad51c70
Pawan Kumar <pawan4super30@gmail.com>
2023-11-27 19:29:06 +0530

    added empty cart image......fixed typo error next.config.js image config....

--- next.config.js
+++ next.config.js
@@ -5,7 +5,7 @@ const nextConfig = {
             "images.pexels.com",
             "img.freepik.com",
             "avatars.githubusercontent.com",
-            "lh3.googleusercontnt.com",
+            "lh3.googleusercontent.com",
         ]
     }
 }
